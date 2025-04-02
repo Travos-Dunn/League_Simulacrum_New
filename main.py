a5acb3851eb9646bb07ec632c3c8acc3b0108941
Travis <111609393+Travos-Dunn@users.noreply.github.com>
2025-04-01 18:07:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ def main() -> None:
     }
 
     version = versions.check_version()
+    files = versions.update_filenames(version)
+
+    item_data = items.check_items(files["item_data"], version)
+    item_list = items.check_item_list(files["item_list"], files["item_data"], version)
+
+    champ_data = champions.check_champs(files["champ_data"], version)
+    champ_list = champions.check_champ_list(files["champ_list"], files["champ_data"], version)
+
+if __name__ == "__main__":
+    main()
